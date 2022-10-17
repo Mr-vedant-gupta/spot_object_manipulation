2de4ec5da4222e8ec34a43fcbbbbf47fda74a841
Mr-vedant-gupta <vedant_gupta@brown.edu>
2022-10-16 21:07:59 -0400

    abstracting code for compatibility with multiple objects

---
+++ helpers/constants.py
@@ -0,0 +1,8 @@
+VELOCITY = 0.5
+FORCE_LIMIT = 50
+OBJECTS = ["drawer"]
+
+kImageSources = [
+    'frontleft_fisheye_image', 'frontright_fisheye_image',
+    'left_fisheye_image', 'right_fisheye_image', 'back_fisheye_image'
+]
