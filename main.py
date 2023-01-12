b734bcad6557035db516ff64a12a7aca44dd9f8a
Sergio Orozco <sorozco0612@yahoo.com>
2023-01-12 16:57:37 -0500

    navigating to object and fiducial orienting integrated together

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ class GraphNavInterface(object):
         fiducial_objects = world_object_client.list_world_objects(
             object_type=request_fiducials).world_objects
 
-        vision_tform_fiducial = fiducial_objects[0].transforms_snapshot.child_to_parent_edge_map['filtered_fiducial_523'].parent_tform_child
+        vision_tform_fiducial = fiducial_objects[0].transforms_snapshot.child_to_parent_edge_map['filtered_fiducial_527'].parent_tform_child
         vision_tform_fiducial = SE3Pose(vision_tform_fiducial.position.x, vision_tform_fiducial.position.y, vision_tform_fiducial.position.z, vision_tform_fiducial.rotation)
 
         seed_tform_fiducial = seed_tform_vision * vision_tform_fiducial
@@ -171,61 +171,73 @@ class GraphNavInterface(object):
             # If no object name is given as input, then return without requesting navigation.
             print("No object provided as a destination for navigate to.")
             return
-        print("aa")
 
         if not args[0][0] in self.vision_model.clusters.keys():
             print(args[0][0] + " not in clusters.")
             return
 
-        # dummy_quat = Quat(w=float(1), x=float(0), y=float(0),z=float(0))
-        print("ab")
-        print(self.vision_model.clusters[args[0][0]])
         seed_T_goal = self.vision_model.clusters[args[0][0]][0] #the list will only have one element
-        # seed_T_goal.rot = dummy_quat
 
-        # goal_tform_pose = SE3Pose(1, 0, 0, dummy_quat)
-
-        # seed_tform_pose = seed_T_goal * goal_tform_pose
-
-        print("ac")
         if not self.toggle_power(should_power_on=True):
             print("Failed to power on the robot, and cannot complete navigate to request.")
             return
 
-        # nav_to_cmd_id = None
-        # # Navigate to the destination.
-        # is_finished = False
-        # while not is_finished:
-        #     # Issue the navigation command about twice a second such that it is easy to terminate the
-        #     # navigation command (with estop or killing the program).
-        #     try:
-        #         nav_to_cmd_id = self._graph_nav_client.navigate_to_anchor(
-        #             seed_tform_pose.to_proto(), 1.0, command_id=nav_to_cmd_id)
-        #     except ResponseError as e:
-        #         print("Error while navigating {}".format(e))
-        #         break
-        #     time.sleep(.5)  # Sleep for half a second to allow for command execution.
-        #     # Poll the robot for feedback to determine if the navigation command is complete. Then sit
-        #     # the robot down once it is finished.
-        #     is_finished = self._check_success(nav_to_cmd_id)
+        nav_to_cmd_id = None
+        # Navigate to the destination.
+        is_finished = False
+        while not is_finished:
 
-        print("ad")
-        ## USING OLD CODE, NEW CODE COMMENTED OUT
+            # Issue the navigation command about twice a second such that it is easy to terminate the
+            # navigation command (with estop or killing the program).
+            try:
 
-        vision_tform_body = bosdyn.client.frame_helpers.get_vision_tform_body(self._robot.get_frame_tree_snapshot())
-        body_tform_vision = vision_tform_body.inverse()
-        print("ae")
-        localization_state = self._graph_nav_client.get_localization_state()
-        seed_tform_body = SE3Pose.from_obj(localization_state.localization.seed_tform_body)
-        
-        if seed_tform_body == None:
-            print("Forgot to upload map")
-        else:
-        
-            seed_tform_vision = seed_tform_body * body_tform_vision
-            vision_tform_seed = seed_tform_vision.inverse()
-            vision_tform_goal = vision_tform_seed * seed_T_goal 
-            self.fetch_model.move_robot_to_location(vision_tform_goal)
+                nav_to_cmd_id = self._graph_nav_client.navigate_to_anchor(
+                    seed_T_goal.to_proto(), 1.0, command_id=nav_to_cmd_id)
+
+            except ResponseError as e:
+                print("Error while navigating {}".format(e))
+                break
+
+            time.sleep(.5)  # Sleep for half a second to allow for command execution.
+
+            # Poll the robot for feedback to determine if the navigation command is complete. Then sit
+            # the robot down once it is finished.
+            is_finished = self._check_success(nav_to_cmd_id)
+
+            localization_state = self._graph_nav_client.get_localization_state()
+            seed_tform_body = SE3Pose.from_obj(localization_state.localization.seed_tform_body)
+
+            distance = math.dist([seed_tform_body.x, seed_tform_body.y], [seed_T_goal.x, seed_T_goal.y])
+
+            if distance <= 2.0:
+                is_finished = True
+
+            if is_finished:
+
+                # This needs to only work for objects that need a fiducial (something like the coffee pot)
+                if "coffee_pot" in args[0][0]:
+
+                    # orient robot based on fiducial marking
+                    vision_tform_body = bosdyn.client.frame_helpers.get_vision_tform_body(self._robot.get_frame_tree_snapshot())
+                    body_tform_vision = vision_tform_body.inverse()
+
+                    seed_tform_vision = seed_tform_body * body_tform_vision
+
+                    world_object_client = self._robot.ensure_client(WorldObjectClient.default_service_name)
+                    # Get all fiducial objects (an object of a specific type).
+                    request_fiducials = [world_object_pb2.WORLD_OBJECT_APRILTAG]
+                    fiducial_objects = world_object_client.list_world_objects(
+                        object_type=request_fiducials).world_objects
+
+                    vision_tform_fiducial = fiducial_objects[0].transforms_snapshot.child_to_parent_edge_map['filtered_fiducial_527'].parent_tform_child
+                    vision_tform_fiducial = SE3Pose(vision_tform_fiducial.position.x, vision_tform_fiducial.position.y, vision_tform_fiducial.position.z, vision_tform_fiducial.rotation)
+
+                    seed_tform_fiducial = seed_tform_vision * vision_tform_fiducial
+
+                    fiducial_tform_goto = SE3Pose(0,0,1,Quat())
+                    seed_tform_goto = seed_tform_fiducial * fiducial_tform_goto
+
+                    self._navigate_to_anchor([seed_tform_goto.position.x, seed_tform_goto.position.y, seed_tform_goto.rotation.to_yaw()])
 
     def _manipulate_object(self, *args):
         self._navigate_to_object(args)
